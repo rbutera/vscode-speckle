9724f1f930bf0aec5f25e93a1c50f1724a514eb7
Rai Butera <rai@rbutera.com>
2021-02-03 16:23:57 +0000

    refactor(activate): scaffold commands

--- src/extension.ts
+++ src/extension.ts
@@ -18,29 +18,49 @@ const openSourceOf = compose(openFile, getSourceFilePath)
 const openTestOf = compose(openFile, getTestFilePath)
 const openTestOrImplementation = ifElse(isTestFile, openSourceOf, openTestOf)
 
+export const switchCommand = () => {
+  const activeFile = vscode.window.activeTextEditor
+  if (activeFile === undefined) return
+
+  switchBetweenSourceAndTest(activeFile.document.uri.fsPath)
+}
+
+export const openCommand = () => {
+  const activeFile = vscode.window.activeTextEditor
+  if (activeFile === undefined) return
+
+  openTestOrImplementation(activeFile.document.uri.fsPath)
+}
+
+export const createCommand = () => {
+  const activeFile = vscode.window.activeTextEditor
+  if (activeFile === undefined) return
+
+  openTestOrImplementation(activeFile.document.uri.fsPath)
+}
+
 export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const switchSubscription = vscode.commands.registerCommand(
     'speckle.switch',
-    () => {
-      const activeFile = vscode.window.activeTextEditor
-      if (activeFile === undefined) return
-
-      switchBetweenSourceAndTest(activeFile.document.uri.fsPath)
-    }
+    switchCommand
   )
 
   const openSubscription = vscode.commands.registerCommand(
     'speckle.open',
-    () => {
-      const activeFile = vscode.window.activeTextEditor
-      if (activeFile === undefined) return
+    openCommand
+  )
 
-      openTestOrImplementation(activeFile.document.uri.fsPath)
-    }
+  const createSubscription = vscode.commands.registerCommand(
+    'speckle.create',
+    createCommand
   )
   // eslint-disable-next-line functional/immutable-data
-  context.subscriptions.push(switchSubscription, openSubscription)
+  context.subscriptions.push(
+    switchSubscription,
+    openSubscription,
+    createSubscription
+  )
 }
