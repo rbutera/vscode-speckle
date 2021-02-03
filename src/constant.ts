05b3dc6cea99e4d7c7b64f7e03eed27712310e31
Rai Butera <rai@rbutera.com>
2021-02-03 11:46:32 +0000

    colocate test files

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,5 @@
-export const TEST_FOLDER_NAME = '__tests__'
-export const TEST_FILE_SUFFIX = 'test'
-export const TEST_FILE_SUFFIXES = ['test', 'spec']
+import * as vscode from "vscode";
+
+export const TEST_FOLDER_NAME = "__tests__";
+export const TEST_FILE_SUFFIX = "test";
+export const TEST_FILE_SUFFIXES = ["test", "spec"];
---
+++ src/createTestFilePath.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+/**
+ * generates an appropriate test file
+ * if user has configured the option speckle.useTestsDirectoryForCreation then will use __tests directory
+ * otherwise uses the current directory
+ * @param file the implementation filename
+ */
+export default function createTestFilePath(file: string): string {}
